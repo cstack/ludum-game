88f27565a4e1dd7c81e356e499e8f6af81bb3227
Matt McNierney <mmcnierney14@gmail.com>
2014-08-23 15:02:23 -0700

    make the camera lag on the z axis

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public GameObject player;
+	private float zPos;
+
+	// Use this for initialization
+	void Start () {
+		zPos = player.transform.position.z - 10;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float dP = (player.transform.position.z - 10) - transform.position.z;
+		zPos += dP * 0.2f;
+		transform.position = new Vector3 (player.transform.position.x + 7, 2, zPos);
+	}
+}

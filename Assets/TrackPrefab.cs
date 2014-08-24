cdb4df67ac2062a9241c368f76a32dfe4986291b
Connor Stack <sirconnorstack@gmail.com>
2014-08-23 18:19:20 -0700

    Generate obstacles.

--- Assets/TrackPrefab.cs
+++ Assets/TrackPrefab.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class TrackPrefab : MonoBehaviour {
+	public GameObject[] obstaclePrefabs;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +10,15 @@ public class TrackPrefab : MonoBehaviour {
 	}
 
 	public void createObstacles () {
-
+		foreach (GameObject obstaclePrefab in obstaclePrefabs) {
+			for (int i = 0; i < 5; i++) {
+				int prefabIndex = Random.Range(0, obstaclePrefabs.Length);
+				GameObject prefab = obstaclePrefabs[prefabIndex];
+				GameObject obstacle = (GameObject) Instantiate(obstaclePrefab);
+				obstacle.transform.parent = transform;
+				obstacle.transform.position = new Vector3(transform.position.x + i * 10 + Random.Range(-5f, 5f), transform.position.y + 1f, transform.position.z);
+			}
+		}
 	}
 	
 	// Update is called once per frame
